3768cd6946e01fedc03fded068400ac499a4cd01
David <83611@siswa.unimas.my>
2023-12-23 12:00:13 +0800

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -1,17 +1,17 @@
+import datetime
 
 class User:
-    def __init__(self,userID,userName,email,password,profilepicture,biodata):
+    def __init__(self, userID, userName, email, password, profilepicture, biodata):
         self.userID = userID
         self.userName = userName
         self.email = email
         self.password = password
         self.profilepicture = profilepicture
         self.biodata = biodata
+        self.notification_settings = NotificationSettings()
 
     def login(self):
-        print("User "+self.userName+" is login")
-class Jobseeker:
-    pass
+        print("User " + self.userName + " is login")
 
 class NotificationSettings:
     def __init__(self):
@@ -25,29 +25,26 @@ class NotificationSettings:
         self.push_notifications = enabled
 
 class Message:
-    def __init__(self, content, timestamp):
+    def __init__(self, content):
         self.content = content
-        self.timestamp = timestamp
+        self.timestamp = datetime.datetime.now() # Current time
         self.is_read = False
 
     def send_message(self, recipient):
-        # Send message to recipient                             LATER
-        pass
+        print("Message sent to: ", recipient)
 
     def received_message(self, sender):
-        # Mark message as received                   LATER      
-        pass
+        self.is_read = False  # Mark message as unread
+        print("Message recieved from: ", sender)
 
-    def view_message(self, message):
-        # Display message content                      LATER            
-        pass
+    def view_message(self):
+        print("Message content: ", self.content)
+        self.is_read = True  # Mark message as read
 
-    def notif_message(self, message):
-        # Display message notification                     LATER
-        pass
+    def notif_message(self):
+        print("New message from: ", self.sender)
 
-
-user_1 = User("82580","Harold","haroldhenry9903@gmail.com","harolH","pic","boy")
+user_1 = User("82580", "Harold", "haroldhenry9903@gmail.com", "harolH", "pic", "boy")
 
 print(user_1.userID)
 print(user_1.userName)
@@ -57,3 +54,8 @@ print(user_1.profilepicture)
 print(user_1.biodata)
 
 user_1.login()
+
+message = Message("Hello, Merry Christmas Mr David.")
+message.send_message("David Lee")
+message.received_message("Trump")
+message.view_message()
